d9627d4d4ec28208cbbd968cc0fa0c418d4aeab2
Gremlin <toxin@211.ru>
2024-09-17 09:59:02 +0700

    Добавил работу камеры и фокусировку на объект

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Transform TR;
+    [SerializeField]
+    private CameraTargetScript cameraTarget;
+
+    private Transform CamTargetTR;
+
+    void Start()
+    {
+        TR = transform;
+        SetCameraTarget(cameraTarget);
+    }
+
+    void Update()
+    {
+        CameraFollow();
+    }
+
+    public void SetCameraTarget(CameraTargetScript _cameraTarget)
+    {
+        cameraTarget = _cameraTarget;
+        SetCameraPosition(cameraTarget.cameraPoint);
+    }
+
+    private void SetCameraPosition(Transform _campoint)
+    {
+        TR.position = _campoint.position;
+        TR.rotation = _campoint.rotation;
+    }
+
+    private void CameraFollow()
+    {
+        TR.position = cameraTarget.cameraPoint.position;
+        TR.rotation = cameraTarget.cameraPoint.rotation;
+    }
+}
